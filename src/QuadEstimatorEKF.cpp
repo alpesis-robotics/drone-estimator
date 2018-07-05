cd51d32ac9072d83462e821742a483e4c18ec05f
K <kwailamchan@hotmail.com>
2018-07-05 11:42:45 +0800

    [master] implemented 10_MagUpdate. 	modified:   config/QuadEstimatorEKF.txt 	modified:   src/QuadEstimatorEKF.cpp

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -313,8 +313,12 @@ void QuadEstimatorEKF::UpdateFromMag(float magYaw)
   //    (you don't want to update your yaw the long way around the circle)
   //  - The magnetomer measurement covariance is available in member variable R_Mag
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+  hPrime(0, 6) = 1;
 
-
+  zFromX(0) = ekfState(6);
+  float diffZ = z(0) - zFromX(0);
+  if (diffZ > F_PI) { zFromX(0) += 2.f * F_PI; }
+  else if (diffZ < -F_PI) { zFromX(0) -= 2.f * F_PI; }
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   Update(z, hPrime, R_Mag, zFromX);
